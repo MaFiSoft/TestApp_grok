1fa3eecfb246705b7fa4b48333a91c87f6315ce8
MaFiSoft <mafi82290@online.de>
2025-05-18 02:57:45 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,17 +32,17 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
+        kotlinCompilerExtensionVersion = "1.5.15" // Korrigiert
     }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.0") // Für Theme.AppCompat
     implementation("androidx.activity:activity-compose:1.9.2")
     implementation(platform("androidx.compose:compose-bom:2024.10.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3:1.3.0")
-    implementation("androidx.compose.compiler:compiler:1.5.15")
+    implementation("androidx.compose.compiler:compiler:1.5.15") // Hinzugefügt
     implementation("androidx.navigation:navigation-compose:2.8.0")
 }
