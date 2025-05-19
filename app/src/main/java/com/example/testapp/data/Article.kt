02937a0dca0782840ea37d30675a80f7af4ed8d1
MaFiSoft <mafi82290@online.de>
2025-05-20 00:36:47 +0200

    Create Article.kt

---
+++ app/src/main/java/com/example/testapp/data/Article.kt
@@ -0,0 +1,10 @@
+package com.example.testapp.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "articles")
+data class Article(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String
+)
