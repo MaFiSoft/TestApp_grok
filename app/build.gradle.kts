973f3bb585f48f18826f06a243e0f7322ca19e11
MaFiSoft <mafi82290@online.de>
2025-05-19 21:35:51 +0200

    Update build.gradle.kts
    Update to compose-bom 2024.06.00, Kotlin 2.0.20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android") version "1.9.25"
+    id("org.jetbrains.kotlin.android") version "2.0.20"
 }
 
 android {
@@ -32,18 +32,18 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
+        kotlinCompilerExtensionVersion = "1.5.17"
     }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.activity:activity-compose:1.9.2")
-    implementation(platform("androidx.compose:compose-bom:2024.10.00"))
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material") // Für Standard-Icons
-    implementation("androidx.navigation:navigation-compose:2.8.0")
-    implementation("androidx.compose.compiler:compiler:1.5.15")
+    implementation("androidx.compose.material:material")
+    implementation("androidx.navigation:navigation-compose:2.8.3")
+    implementation("androidx.compose.compiler:compiler:1.5.17")
 }
