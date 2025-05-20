8e59bca9aa53612ad7291f366d04f5cb897c0f8b
MaFiSoft <mafi82290@online.de>
2025-05-20 02:08:54 +0200

    Delete app/src/main/java/com/example/testapp/data directory

--- app/src/main/java/com/example/testapp/data/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.testapp.data
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [Article::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun articleDao(): ArticleDao
-}
