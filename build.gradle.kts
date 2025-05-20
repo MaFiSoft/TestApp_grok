21367cc5422b22fb4814d2eb8470c5872ebb64b6
MaFiSoft <mafi82290@online.de>
2025-05-20 22:00:15 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,12 @@
+// Stand: 2025-05-20_21:45
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 plugins {
     id("com.android.application") version "8.3.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
