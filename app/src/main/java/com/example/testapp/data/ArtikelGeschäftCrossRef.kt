93f06bd566ab80e684a53b8ea2a5d9d2c933264e
MaFiSoft <mafi82290@online.de>
2025-05-21 21:58:09 +0200

    Update ArtikelGeschäftCrossRef.kt

--- app/src/main/java/com/example/testapp/data/ArtikelGeschäftCrossRef.kt
+++ app/src/main/java/com/example/testapp/data/ArtikelGeschäftCrossRef.kt
@@ -1,1 +1,29 @@
+// Stand: 2025-05-21_22:30
+// app/src/main/java/com/example/testapp/data/ArtikelGeschäftCrossRef.kt
+package com.example.testapp.data
 
+import androidx.room.Entity
+import androidx.room.ForeignKey
+
+@Entity(
+    tableName = "artikel_geschäft",
+    primaryKeys = ["artikelId", "geschäftId"],
+    foreignKeys = [
+        ForeignKey(
+            entity = Artikel::class,
+            parentColumns = ["id"],
+            childColumns = ["artikelId"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Geschäft::class,
+            parentColumns = ["id"],
+            childColumns = ["geschäftId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
+data class ArtikelGeschäftCrossRef(
+    val artikelId: Int,
+    val geschäftId: Int
+)
