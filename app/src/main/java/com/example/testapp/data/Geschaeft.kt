2c94b2e362996cdd9486c3734fdd616b2415727b
MaFiSoft <mafi82290@online.de>
2025-05-21 22:11:48 +0200

    Update Geschaeft.kt

--- app/src/main/java/com/example/testapp/data/Geschaeft.kt
+++ app/src/main/java/com/example/testapp/data/Geschaeft.kt
@@ -1,12 +1,12 @@
 // Stand: 2025-05-21_22:30
-// app/src/main/java/com/example/testapp/data/Geschäft.kt
+// app/src/main/java/com/example/testapp/data/Geschaeft.kt
 package com.example.testapp.data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "geschäfte")
-data class Geschäft(
+@Entity(tableName = "geschaefte")
+data class Geschaeft(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String
 )
