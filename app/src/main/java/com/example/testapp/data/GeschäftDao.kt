762eda138b07dd5b2fddfe98de2b439a58bda225
MaFiSoft <mafi82290@online.de>
2025-05-21 22:03:34 +0200

    Update GeschäftDao.kt

--- app/src/main/java/com/example/testapp/data/GeschäftDao.kt
+++ app/src/main/java/com/example/testapp/data/GeschäftDao.kt
@@ -1,1 +1,20 @@
+// Stand: 2025-05-21_22:30
+// app/src/main/java/com/example/testapp/data/GeschäftDao.kt
+package com.example.testapp.data
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface GeschäftDao {
+    @Insert
+    suspend fun insert(geschäft: Geschäft)
+
+    @Query("SELECT * FROM geschäfte")
+    fun getAllGeschäfte(): Flow<List<Geschäft>>
+
+    @Query("DELETE FROM geschäfte WHERE id = :geschäftId")
+    suspend fun delete(geschäftId: Int)
+}
