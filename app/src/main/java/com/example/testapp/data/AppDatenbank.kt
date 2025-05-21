7a721094bceb843c2c3155469d5e0753f5905fd8
MaFiSoft <mafi82290@online.de>
2025-05-21 23:41:05 +0200

    Update AppDatenbank.kt

--- app/src/main/java/com/example/testapp/data/AppDatenbank.kt
+++ app/src/main/java/com/example/testapp/data/AppDatenbank.kt
@@ -5,7 +5,7 @@ package com.example.testapp.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Artikel::class, Kategorie::class, Geschaeft::class, ArtikelGeschaeftCrossRef::class], version = 1)
+@Database(entities = [Artikel::class, Kategorie::class, Geschaeft::class, ArtikelGeschaeftCrossRef::class], version = 1, exportSchema = true)
 abstract class AppDatenbank : RoomDatabase() {
     abstract fun artikelDao(): ArtikelDao
     abstract fun kategorieDao(): KategorieDao
