7f7fdb629127991d5e7258e794c6f1758d4c1b62
MaFiSoft <mafi82290@online.de>
2025-05-21 21:57:03 +0200

    Update Kategorie.kt

--- app/src/main/java/com/example/testapp/data/Kategorie.kt
+++ app/src/main/java/com/example/testapp/data/Kategorie.kt
@@ -1,1 +1,12 @@
+// Stand: 2025-05-21_22:30
+// app/src/main/java/com/example/testapp/data/Kategorie.kt
+package com.example.testapp.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "kategorien")
+data class Kategorie(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String
+)
