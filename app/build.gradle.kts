16b48760055209be9f0667230db74d3bd38e5844
MaFiSoft <mafi82290@online.de>
2025-05-22 02:31:10 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application") version "8.5.2"
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0"
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.24" apply false
+    id("com.google.devtools.ksp") version "2.0.0-1.0.24"
 }
 
 android {
